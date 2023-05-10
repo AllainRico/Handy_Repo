710f9e9034d9512725f56839910ad706ef107db4
allainrico <allainrico@gmail.com>
2023-05-10 19:14:59 +0800

    Changes on the Signup: added Check Unique, length, and uppercase constraints.

--- HandyPH/signupForm.cs
+++ HandyPH/signupForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,90 @@ namespace HandyPH
 
         private void signupform_signupButton_Click(object sender, EventArgs e)
         {
-            userHome userhome = new userHome();
-            userhome.Show();
-            this.Hide();
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = ("Data Source=DESKTOP-SKI34QJ\\SQLEXPRESS;Initial Catalog=handymandb;Integrated Security=True");
+            //change this shit kay ma error ni 
+
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("select * from tblUsers where username='" + signupform_usernametextBox.Text + "'", con);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+
+            //requirements for password!!! (Still subject for revision)
+            //1>must be > 8
+            //2>must have uppercase
+
+            if (dt.Rows.Count == 0) //checks if name is already taken
+            {
+                if (signupform_usernametextBox.Text == "" || signupform_passwordtextBox.Text == "") //if fields are empty
+                {
+                    MessageBox.Show("Fields are empty!", "ERROR: 1");
+                }
+                else
+                {
+                    if (signupform_passwordtextBox.TextLength < 7)
+                    {
+                        MessageBox.Show("Password length is lesser than 8", "ERROR: 2");
+                    }
+                    else
+                    {
+                        if (signupform_confirmpasswordtextBox.Text != signupform_passwordtextBox.Text)
+                        {
+                            MessageBox.Show("password does not match", "ERROR: 3");
+                        }
+                        else
+                        {
+                            bool hasUpperCase = false;
+                            bool hasLowerCase = false;
+
+                            foreach (char c in signupform_passwordtextBox.Text)
+                            {
+                                if (char.IsUpper(c))
+                                {
+                                    hasUpperCase = true;
+                                }
+                                else if (char.IsLower(c))
+                                {
+                                    hasLowerCase = true;
+                                }
+                            }
+                            if (hasUpperCase && hasLowerCase)
+                            {
+                                //string encpass = Aescryp.(signupform_passwordtextBox.Text);
+                                string encpass = signupform_passwordtextBox.Text;
+
+                                String st = "INSERT INTO usersTable(username,pass) values (@username, @pass)";
+                                SqlCommand cmd1 = new SqlCommand(st, con);
+                                cmd1.Parameters.AddWithValue("@username", signupform_usernametextBox.Text);
+                                cmd1.Parameters.AddWithValue("@pass", encpass);
+                                cmd1.ExecuteNonQuery();
+                                SqlCommand cmd2 = new SqlCommand("INSERT INTO borrowersTable(username) VALUES (@username)", con);
+                                cmd2.Parameters.AddWithValue("@username", signupform_usernametextBox.Text);
+                                cmd2.ExecuteNonQuery();
+                                con.Close();
+
+                                MessageBox.Show("Account created successfully! - Please input credentials in the Login Form", "Successful!");
+
+                                this.Hide();
+                                loginForm login = new loginForm();
+                                login.Show();
+                            }
+                            else
+                            {
+                                MessageBox.Show("Password does not have an uppercase", "ERROR: 4");
+                            }
+
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Username already taken");
+            }
+
         }
     }
 }
